99860eadf655096e31a8ee29e58aab3873352bbd
Daniel Pellegrini <danpellegrini63@gmail.com>
2025-11-01 17:05:03 -1000

    send up package state

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+// Service Worker for LDAH Progressive Web App
+const CACHE_NAME = 'ldah-v1';
+const urlsToCache = [
+  '/index.html',
+  '/events.html',
+  '/faq.html',
+  '/volunteer.html',
+  '/contact.html',
+  'https://cdn.tailwindcss.com',
+  'https://danpoahu.github.io/LDAH/logo_transparent.png',
+  'https://danpoahu.github.io/LDAH/background.png'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
